dc47345ba1f99c0d336da8527fa69ab745389d41
Orlando <espadachinkinno@gmail.com>
2025-06-08 22:15:03 -0600

    Add technology icons and create technology section
    - Added SVG icons for Next.js, Node.js, React, SQL Server, Strapi, TypeScript, and C#.
    - Updated the header to include a link to the Technologies section.
    - Created a new component for technology cards (TarjetaTecnologia.astro) to display technology icons and titles.
    - Introduced a new section (Tecnologias.astro) to showcase the technologies used, utilizing the new card component.
    - Updated the JavaScript file (tecnologias.js) to include the new technology icons and organized them into frontend, backend, and tools categories.
    - Modified the main index page to include the new Technologies section.
    - Cleaned up unnecessary whitespace in the Proyectos section.

--- src/js/tecnologias.js
+++ src/js/tecnologias.js
@@ -1,3 +1,18 @@
+import html from '../assets/iconos/html.svg';
+import css from '../assets/iconos/css.svg';
+import javascript from '../assets/iconos/javascript.svg';
+import react from '../assets/iconos/react.svg';
+import astro from '../assets/iconos/astro.svg';
+import nodejs from '../assets/iconos/nodejs.svg';
+import nextjs from '../assets/iconos/nextjs.svg';
+import mysql from '../assets/iconos/mysql.svg';
+import express from '../assets/iconos/express.svg';
+import sqlserver from '../assets/iconos/sqlserver.svg';
+import git from '../assets/iconos/git.svg';
+import strapi from '../assets/iconos/strapi.svg';
+import typescript from '../assets/iconos/typescript.svg';
+import csharp from '../assets/iconos/csharp.svg';
+
 export const iconos = {
     "HTML": "html",
     "CSS": "css",
@@ -9,3 +24,26 @@ export const iconos = {
     "MySql": "mysql",
     "Express": "express",
 }
+
+export const iconosTecnologias = {
+  frontend: [
+    { titulo: "HTML", icono: html },
+    { titulo: "CSS", icono: css },
+    { titulo: "JavaScript", icono: javascript },
+    { titulo: "React", icono: react },
+    { titulo: "Astro", icono: astro },
+    { titulo: "NextJs", icono: nextjs },
+    { titulo: "TypeScript", icono: typescript },
+  ],
+  backend: [
+    { titulo: "NodeJs", icono: nodejs },
+    { titulo: "Express", icono: express },
+    { titulo: "MySql", icono: mysql },
+    { titulo: "SQL Server", icono: sqlserver },
+    { titulo: "Strapi", icono: strapi },
+    { titulo: "C#", icono: csharp },
+  ],
+  herramientas: [
+    { titulo: "Git", icono: git },
+  ],
+};
